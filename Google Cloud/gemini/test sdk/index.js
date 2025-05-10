13b26d9f581c2ed0bf7bfb9b93cc1257c7d7572b
macelodev23 <marcelo-oliveira.mo@academico.ifpb.edu.br>
2025-05-10 00:53:51 -0300

    ...

---
+++ Google Cloud/gemini/test sdk/index.js
@@ -0,0 +1,21 @@
+import {GoogleGenAI} from '@google/genai'
+import dotenv from 'dotenv'
+
+dotenv.config(); 
+const key = process.env.APIKEY;
+
+
+const ai = new GoogleGenAI({apiKey:key})
+
+async function main() {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: "olá ,tudo bem",
+        config: {
+          systemInstruction: "vocé é assistente virtual",
+        },
+      });
+   console.log(response.text);
+}
+
+await main();
